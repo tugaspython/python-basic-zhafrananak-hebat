35a3767b3ce07601e404481c5be1d746dcf69eeb
zhafrananak-hebat <zhafranajah06@gmail.com>
2026-05-21 10:23:26 +0700

    Update jawaban_mahasiswa.py

--- jawaban_mahasiswa.py
+++ jawaban_mahasiswa.py
@@ -9,25 +9,25 @@ def tambah(a, b):
     Fungsi untuk menjumlahkan a dan b.
     """
     # Tulis kode Anda di bawah ini
-    pass
+    return a + b
 
 def kurang(a, b):
     """
     Fungsi untuk mengurangkan a dengan b.
     """
     # Tulis kode Anda di bawah ini
-    pass
+    return a - b
 
 def kali(a, b):
     """
     Fungsi untuk mengalikan a dan b.
     """
     # Tulis kode Anda di bawah ini
-    pass
+    return a * b
 
 def bagi(a, b):
     """
     Fungsi untuk membagi a dengan b.
     """
     # Tulis kode Anda di bawah ini
-    pass
+    return a / b
